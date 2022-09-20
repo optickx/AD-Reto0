af80d7b702fa0d9f705c772a86c432976918f71e
2dam <2dam@TartangaLH.eus>
2022-09-20 10:08:56 +0200

    Prepared statements de movements

--- src/model/dao/ModelImplementation.java
+++ src/model/dao/ModelImplementation.java
@@ -1,10 +1,19 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.Modelable;
 import obj.*;
 
-public class ModelImplementation implements Modelable {
-
+public class ModelImplementation extends SQLAccess implements Modelable {
+    private PreparedStatement stmt;
+    
+    final String addMovement = "INSERT INTO MOVEMENT VALUES (?,?,?,?,?,?)";
+    final String checkMovement = "SELECT M.* FROM MOVEMENT M,ACCOUNT A WHERE A.ID=? AND M.ACCOUNT_ID=A.ID";
+    
     @Override
     public void createCustomer(Customer pCustomer) {
         // TODO Auto-generated method stub
@@ -32,7 +41,15 @@ public class ModelImplementation implements Modelable {
 
     @Override
     public void addMovement(Movement pMovement) {
-        // TODO Auto-generated method stub
+       //Open connection
+        openConnection();
+        
+        try {
+            stmt = con.prepareStatement(addMovement);
+        } catch (SQLException ex) {
+            Logger.getLogger(ModelImplementation.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
         
     }
 
